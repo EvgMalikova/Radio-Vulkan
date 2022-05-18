debcb526f2b2f253ea2b262d58495489cae7f648
EvgeniyaMalikova <evgeniya.malikova@port.ac.uk>
2022-05-18 14:43:54 +0100

    WSRTI files

---
+++ WSRTI/syncqueue/build/CMakeFiles/syncqueue-test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/syncqueue-test.dir/test/SyncQueueTest.cpp.o"
+  "syncqueue-test"
+  "syncqueue-test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/syncqueue-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
